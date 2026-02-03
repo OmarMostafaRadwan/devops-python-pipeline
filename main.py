246abe6f9a4fe47b4d413461d575f7a0b3407bf0
OmarMostafaRadwan <omar12mos12radwan@gmail.com>
2026-02-03 17:11:54 +0200

    3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Pipeline is Operational! Version 1.0"
+    return "Pipeline is Operational! Version 22.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
